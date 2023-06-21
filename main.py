0f14afdc02acd7dc43c6b9c306a3112a83f10669
Rodrigo Rocha <rocha.rodrigogomes@gmail.com>
2023-06-21 09:31:08 -0300

    filter dataframes

--- main.py
+++ main.py
@@ -11,15 +11,73 @@ def csvs_from_folder(folder_path):
         df = pd.read_csv(file_path)
         dataframes.append(df)
 
-    concatenated_df = pd.concat(dataframes, ignore_index=True)
+    return pd.concat(dataframes, ignore_index=True)
 
-    return concatenated_df
+def filter_dataframe_by_tuples(df, filters):
+    filtered_df = df.copy()
+    
+    for column, value in filters:
+        filtered_df = filtered_df[filtered_df[column] == value]
+    
+    return filtered_df
+
+
+def read_filters_from_file(file_path):
+    filters = []
+    
+    with open(file_path, 'a') as file:
+        pass
+    
+    with open(file_path, 'r') as file:
+        for line in file:
+            line = line.strip()
+            if line:
+                column, value = line.split('=')
+                filters.append((column.strip(), value.strip()))
+    
+    return filters
 
 if __name__ == '__main__':
-    if len(sys.argv) < 2:
+    if len(sys.argv) < 3:
         print('Please provide the folder path as an argument.')
         sys.exit(1)
 
     folder_path = sys.argv[1]
     concatenated_dataframe = csvs_from_folder(folder_path)
-    print(concatenated_dataframe)
+
+    dataframes_dict = {}  # Dictionary to store DataFrames
+    exit_program = False
+
+    while not exit_program:
+        command = input("Enter a command (or 'exit' to quit): ")
+
+        if command == 'exit':
+            exit_program = True
+
+        elif command.startswith('concatenate'):
+            command_parts = command.split()
+            if len(command_parts) != 3:
+                print("Invalid command format. Please provide 'concatenate <path> <name>'.")
+                continue
+
+            path = command_parts[1]
+            name = command_parts[2]
+
+            try:
+                df = pd.read_csv(path)
+                dataframes_dict[name] = df
+                print(f"DataFrame '{name}' concatenated and saved successfully.")
+
+            except FileNotFoundError:
+                print("File not found. Please provide a valid file path.")
+
+        # Add more commands here...
+
+        else:
+            print("Unknown command. Please try again.")
+
+    # Example usage of the filtered_dataframe_by_tuples function
+    filters_file = 'filter.txt'
+    filters = read_filters_from_file(filters_file)
+    filtered_df = filter_dataframe_by_tuples(concatenated_dataframe, filters)
+    print(filtered_df)
